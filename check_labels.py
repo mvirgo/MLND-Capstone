acb2b90dff599d9c59cd40e9cc03278bdbf379bc
Michael Virgo <mvirgs44@gmail.com>
2017-05-02 21:07:26 -0700

    feat: updated labels & combining
    Added updated files for combining image data, adding image rotations and horizontal flips, and "labeling" the road lane label images from the polynomial coefficients previously calculated. Also, added the primary dist and mtx files used to calibrate for my iPhone cameras and the primary perspective matrix used.

--- check_labels.py
+++ check_labels.py
@@ -1,57 +1,63 @@
-""" This file pulls in road images (created by `load_road_images.py`
-and respective labels (that have been created by `make_labels.py`). 
-Using those labels, it draws on the lane area onto the image. This file 
-is for the purpose of checking whether the labels appear fairly accurate,
-or whether further manual manipulation is needed of the lines being fed 
-to create the labels for a given image.
+""" This file uses the original polynomial coefficient labels
+in order to create an image with a green lane to overlay on
+the original road image. The file originally saved down any
+re-combined images in order to check whether the labels were
+appropriate; the current version saves them all to a pickle file
+for usage as the true labels for the fully convolutional neural
+network for training.
 """
 
 import numpy as np
 import cv2
 import pickle
-import matplotlib.image as mpimg
+from scipy.misc import imresize
 
-# Import road images and labels pickle files
-train_images = pickle.load(open( "road_images.p", "rb" ))
+# Load all training images
+train_images = pickle.load(open( "good_road_images.p", "rb" ))
+
+# Load all polynomial coefficient labels
 labels = pickle.load(open( "lane_labels.p", "rb" ))
 
-# Load the perspective transform (since the labels are on the transformed images)
+# Load the perspective transformation matrix, and calculate the inverse
 perspective_M = pickle.load(open('perspective_matrix.p', "rb" ))
 Minv = np.linalg.inv(perspective_M)
 
-# Creates an image in a necessary shape for lane lines to be drawn before inverse transform
 other = cv2.cvtColor(np.copy(train_images[0]), cv2.COLOR_RGB2GRAY)
 
 lane_lines = []
 
-# Iterate through each image, using labels to plot lane lines and draw the lane based on those boundaries
 for n in range(len(train_images)):
-    # Left line coefficients are first three labels, right line is last three
+    # Left line is first three coefficients, right line is the other three
     left_fit = labels[n][0:3]
     right_fit = labels[n][3:]
-    
-    # Fit the labels to a polynomial equation for each line
+
+    # Fit polynomials
     fity = np.linspace(0, other.shape[0]-1, other.shape[0])
     fit_leftx = left_fit[0]*fity**2 + left_fit[1]*fity + left_fit[2]
     fit_rightx = right_fit[0]*fity**2 + right_fit[1]*fity + right_fit[2]
-    
-    # Creates blank image to re-draw lines on
+
+    # Create a space to draw the lane image labels
     warp_zero = np.zeros_like(other).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-    
-    # Points making up the lines
+
+    # Points to fill on the lane image label
     pts_left = np.array([np.transpose(np.vstack([fit_leftx, fity]))])
     pts_right = np.array([np.flipud(np.transpose(np.vstack([fit_rightx, fity])))])
     pts = np.hstack((pts_left, pts_right))
-    
-    # Drawing the detected lane down based on the points
+
+    # Fill the space
     cv2.fillPoly(color_warp, np.int_([pts]), (0,255,0))
-    
-    # Unwarps the detected lane from the bird's eye view to regular image view
+
+    # Warp back to the non-bird's eye view of the road
     newwarp = cv2.warpPerspective(color_warp, Minv, (train_images[n].shape[1], train_images[n].shape[0]))
-    
-    # Draws the lane back onto the original road image
-    result = cv2.addWeighted(train_images[n], 1, newwarp, 0.3, 0)
-    
-    # Save down the files (to a folder named 'labelled') for review of reasonableness of the lane detection
-    mpimg.imsave("labelled//labelled%d.jpg" % (n), result)
+
+    # Using 80x160x3 images for training; also only needs 'G' channel
+    # Lane is drawn in 'G' so the others are unneeded
+    newwarp = imresize(newwarp, (80, 160, 3))
+    newwarp = newwarp[:,:,1]
+    newwarp = newwarp[:,:,None]
+
+    lane_lines.append(newwarp)
+
+# Save labels to pickle file
+pickle.dump(lane_lines,open('image_lane_labels.p', "wb" ))
--- combine_and_normal.py
+++ combine_and_normal.py
@@ -1,92 +1,144 @@
-""" This file merges my image files and label files, and 
-normalizes the lane labels with StandardScaler. These are
-saved so that the labels can later be  de-normalized after
-prediction. It also adds rotations to the original images 
-that are outside of the middle of the distribution of labels
-(based off histograms by each label) to help the model 
-generalize away from straight lines better.
-"""
+''' This file merges my image files and label files.
+It also resizes the training images. The file also
+generates additional image data based on images outside
+the very middle of the original polynomial coefficient
+distributions in three waves, and adds rotations to both the
+road image and corresponding lane image label. Also, for all
+road images, an extra is added by horizontally flipping both
+the image and its lane image label.
+'''
 
 import numpy as np
 import cv2
 import pickle
 from scipy.misc import imresize
 from scipy import ndimage
-from sklearn.preprocessing import StandardScaler
+
+def find_to_rotate(labels, ignore_range):
+    """Finds the label numbers in the loaded data which
+    fall outside a given range.
+    """
+    # List for each coefficient
+    labels_by_coeff = [[],[],[],[],[],[]]
+
+    # Iterate through each label and append to list
+    for label in labels:
+        for n in range(len(label)):
+            labels_by_coeff[n].append(label[n])
+
+    images_to_rotate = []
+
+    # Append to the list for all outside of the ranges above
+    for n in range(len(labels)):
+        for x in range(len(labels[0])):
+            if labels[n][x] < ignore_range[x][0] or labels[n][x] > ignore_range[x][1]:
+                images_to_rotate.append(n)
+
+    # Make the list into unique values only (i.e. only one for each image)
+    images_to_rotate = np.unique(images_to_rotate)
+
+    return images_to_rotate
+
+def rotate_images(train_images, pic_labels, images_to_rotate, angles):
+    """ Adds "fake" training data using rotations of the original
+    road images and lane image labels.
+    """
+    # Small angles to rotate the images
+    more_X = []
+    more_y = []
+
+    # Create new images (with same label as original) with the rotation angles above
+    for n in (images_to_rotate):
+        for l in range(len(angles)):
+            more_X.append(ndimage.rotate(train_images[n], angles[l], reshape = False))
+            more_y.append(ndimage.rotate(pic_labels[n], angles[l], reshape = False))
+
+    # Combine the old training images and labels with the new data
+    train_images = train_images + more_X
+    pic_labels = pic_labels + more_y
+
+    return train_images, pic_labels
+
+def horiz_flip(train_images, pic_labels):
+    """Adds additional "fake" training data by horizontally flipping
+    the road images and lane image labels, after additional were already added
+    by the image rotations.
+    """
+    # Hold images prior to appending at once
+    more_X = []
+    more_y = []
+
+    # Flip images horizontally - note that doing so in the keras generator would not flip labels
+    for i in range(len(train_images)):
+        more_X.append(np.fliplr(train_images[n]))
+        more_y.append(np.fliplr(pic_labels[n]))
+
+    # Combine the old training images and labels with the new data
+    train_images = train_images + more_X
+    pic_labels = pic_labels + more_y
+
+    return train_images, pic_labels
 
 # Load in all training images
-train_images = pickle.load(open( "good_perspective_images.p", "rb" ))
-train_images_curve = pickle.load(open( "good_perspective_images_curve.p", "rb" ))
+train_images = pickle.load(open( "good_road_images.p", "rb" ))
+train_images_curve = pickle.load(open( "good_road_images_curve.p", "rb" ))
+train_images_u = pickle.load(open( "good_u_road_images.p", "rb" ))
 
 # Combine into one
-train_images = train_images + train_images_curve
+train_images = train_images + train_images_curve + train_images_u
 
 # Clear out memory for the unneeded data
 train_images_curve = []
+train_images_u = []
 
-# Downsize, grayscale and normalize training images
-# Note that Keras expects a third dimension, which grayscaling had removed
+# Downsize training images
 for n in range(len(train_images)):
-    new_image = imresize(train_images[n], (45, 80, 3))
-    new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2GRAY)
-    new_image = new_image[:,:,None]
-    new_image = (new_image / 255) * .8 + 1
+    new_image = imresize(train_images[n], (80, 160, 3))
     train_images[n] = new_image
 
-# Load in all labels
+# Load in all labels (coefficients)
 labels = pickle.load(open( "lane_labels.p", "rb" ))
 labels_curve = pickle.load(open( "lane_labels_curve.p", "rb" ))
+labels_u = pickle.load(open( "u_lane_labels.p", "rb" ))
 
 # Combine into one
-labels = labels + labels_curve
+labels = labels + labels_curve + labels_u
 
 # Clear out unneeded data
 labels_curve = []
+labels_u = []
 
-# List for each coefficient
-labels_by_coeff = [[],[],[],[],[],[]]
+# Load in all picture labels (redrawn lines in G color)
+pic_labels = pickle.load(open( "image_lane_labels.p", "rb" ))
+pic_labels_curve = pickle.load(open( "image_lane_labels_curve.p", "rb" ))
+pic_labels_u = pickle.load(open( "image_u_lane_labels.p", "rb" ))
 
-# Iterate through each label and append to list
-for label in labels:
-    for n in range(len(label)):
-        labels_by_coeff[n].append(label[n])
-        
-# Below ranges are based on histograms of the distribution of the above
-# These ignore the middle parts of the distribution to focus on rarer values
-fine_range = ((-.00088,.000594),(-1.29,0.74),(-144,724),(-.00074,.00079),(-1.36,1),(304,1223))
+# Combine into one
+pic_labels = pic_labels + pic_labels_curve + pic_labels_u
 
-images_to_rotate = []
-
-# Append to the list for all outside of the ranges above
-for n in range(len(labels)):
-    for x in range(len(labels[0])):
-        if labels[n][x] < fine_range[x][0] or labels[n][x] > fine_range[x][1]:
-            images_to_rotate.append(n)
-
-# Make the list into unique values only (i.e. only one for each image)
-images_to_rotate = np.unique(images_to_rotate)
+# Clear out unneeded data
+pic_labels_curve = []
+pic_labels_u = []
 
 # Small angles to rotate the images
 angles = [-1, 1, -2, 2]
-more_X = []
-more_y = []
 
-# Create new images (with same label as original) with the rotation angles above
-for n in (images_to_rotate):
-    for l in range(len(angles)):
-        more_X.append(ndimage.rotate(train_images[n], angles[l], reshape = False))
-        more_y.append(labels[n])
+# Ranges to ignore for adding additional rotations
+# The first has roughly 400 per coeff outside the range, second ~100, and third ~30
+fine_range = (((-.00067,.000383),(-0.99,0.73),(-35,650),(-.00052,.00058),(-1.06,0.75),(419,1240)),
+              ((-.00130,.00165),(-2.1,1.8),(-290,1165),(-.0014,.00186),(-2.5,1.86),(-40,1700)),
+              ((-.00193,.00270),(-3.59,2.46),(-578,1590),(-.002,.00298),(-3.7,3),(-270,2372)))
 
-# Combine the old training images and labels with the new data
-train_images = train_images + more_X
-labels = labels + more_y
+for n in range(len(fine_range)):
+    images_to_rotate = find_to_rotate(labels, fine_range[n])
+    train_images, pic_labels = rotate_images(train_images, pic_labels, images_to_rotate, angles)
 
-# The below code will normalize the labels by each coefficient.
-label_scaler = StandardScaler()
-labels = label_scaler.fit_transform(labels)
+# Adds horizontal flips of each image and lane imagelabel
+train_images, pic_labels = horiz_flip(train_images, pic_labels)
 
-# Save images, labels, and scaler to pickle files
-# Note that the scaler will be needed to revert predicted labels to normal
-pickle.dump(train_images,open('full_perspect_train.p', "wb" ))
-pickle.dump(labels,open('full_labels.p', "wb" ))
-pickle.dump(label_scaler,open('scaler.p',"wb"))
+# Save images to pickle file
+pickle.dump(train_images,open('full_CNN_train.p', "wb" ))
+train_images = []
+
+# Save labels to pickle file
+pickle.dump(pic_labels,open('full_CNN_labels.p', "wb" ))
