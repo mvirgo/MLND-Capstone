df9a2117d2d55c15937d6b9c90b106a06be2c8ad
Michael Virgo <mvirgs44@gmail.com>
2017-07-21 07:35:47 -0700

    feat: re-add coeff scaler file
    This file combines and normalizes the images and coefficient labels, and also outputs a scaler.p file that normalized the labels. After training, this scaler file MUST be used to convert the neural network output back to true values.

---
+++ combine_and_normal_old.py
@@ -0,0 +1,92 @@
+""" This file merges my image files and label files, and 
+normalizes the lane labels with StandardScaler. These are
+saved so that the labels can later be  de-normalized after
+prediction. It also adds rotations to the original images 
+that are outside of the middle of the distribution of labels
+(based off histograms by each label) to help the model 
+generalize away from straight lines better.
+"""
+
+import numpy as np
+import cv2
+import pickle
+from scipy.misc import imresize
+from scipy import ndimage
+from sklearn.preprocessing import StandardScaler
+
+# Load in all training images
+train_images = pickle.load(open( "good_perspective_images.p", "rb" ))
+train_images_curve = pickle.load(open( "good_perspective_images_curve.p", "rb" ))
+
+# Combine into one
+train_images = train_images + train_images_curve
+
+# Clear out memory for the unneeded data
+train_images_curve = []
+
+# Downsize, grayscale and normalize training images
+# Note that Keras expects a third dimension, which grayscaling had removed
+for n in range(len(train_images)):
+    new_image = imresize(train_images[n], (45, 80, 3))
+    new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2GRAY)
+    new_image = new_image[:,:,None]
+    new_image = (new_image / 255) * .8 + 1
+    train_images[n] = new_image
+
+# Load in all labels
+labels = pickle.load(open( "lane_labels.p", "rb" ))
+labels_curve = pickle.load(open( "lane_labels_curve.p", "rb" ))
+
+# Combine into one
+labels = labels + labels_curve
+
+# Clear out unneeded data
+labels_curve = []
+
+# List for each coefficient
+labels_by_coeff = [[],[],[],[],[],[]]
+
+# Iterate through each label and append to list
+for label in labels:
+    for n in range(len(label)):
+        labels_by_coeff[n].append(label[n])
+        
+# Below ranges are based on histograms of the distribution of the above
+# These ignore the middle parts of the distribution to focus on rarer values
+fine_range = ((-.00130,.00165),(-2.1,1.8),(-290,1165),(-.0014,.00186),(-2.5,1.86),(-40,1700))
+
+images_to_rotate = []
+
+# Append to the list for all outside of the ranges above
+for n in range(len(labels)):
+    for x in range(len(labels[0])):
+        if labels[n][x] < fine_range[x][0] or labels[n][x] > fine_range[x][1]:
+            images_to_rotate.append(n)
+
+# Make the list into unique values only (i.e. only one for each image)
+images_to_rotate = np.unique(images_to_rotate)
+
+# Small angles to rotate the images
+angles = [-1, 1, -2, 2, -3, 3, -4, 4, -5, 5]
+more_X = []
+more_y = []
+
+# Create new images (with same label as original) with the rotation angles above
+for n in (images_to_rotate):
+    for l in range(len(angles)):
+        more_X.append(ndimage.rotate(train_images[n], angles[l], reshape = False))
+        more_y.append(labels[n])
+
+# Combine the old training images and labels with the new data
+train_images = train_images + more_X
+labels = labels + more_y
+
+# The below code will normalize the labels by each coefficient.
+label_scaler = StandardScaler()
+labels = label_scaler.fit_transform(labels)
+
+# Save images, labels, and scaler to pickle files
+# Note that the scaler will be needed to revert predicted labels to normal
+pickle.dump(train_images,open('full_perspect_train.p', "wb" ))
+pickle.dump(labels,open('full_labels.p', "wb" ))
+pickle.dump(label_scaler,open('scaler.p',"wb"))
